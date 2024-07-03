582acaddceb963795cac4ed8bfa2f820836233b1
Edson Yamble <edsonyamble2@gmail.com>
2024-07-03 13:39:11 +0700

    module event

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const fs = require('fs')
-fs.readFile('./text.txt', 'utf8', (error, data) => {
-	fs.mkdir('./file', () => {
-		fs.writeFile('./file/text2.txt', `${data} new test`, error => {
-			error ? console.log(error) : null
-		})
-	})
-})
-setTimeout(() => {
-	if (fs.existsSync('./file/text2.txt')) {
-		fs.unlink('./file/text2.txt', () => {console.log('delete text2.txt')})
-	}
-}, 4000)
-setTimeout(() => {
-	if (fs.existsSync('./file')) {
-		fs.rmdir('./file', () => {console.log('delete file')})
-	}
-}, 8000)
+//module sabiti or module event это сигнал который сообшает что что то произошло
+// const EventEmitter = require('events') //это где будем создавать экземпля класс для отслежевание разных собиты
+const Logger = require('./log')
+const logger = new Logger()
+// const emitter = new EventEmitter()
+// 1 argument
+// emitter.on('some_event', text => {
+// 	console.log(text)
+// }) //create event
+// emitter.emit('some_event', 'call event!!!!yes') //call event
+// any arguments
+// emitter.on('some_event', args => {
+// 	const { id, text } = args
+// 	console.log(id, text)
+// }) //create event
+// emitter.emit('some_event', { id: 1, text: 'call event!!!!yes' }) //call event
+// emitter.emit('some_event', { id: 2, text: 'second call event' })
+logger.on('some_event', args => {
+	const { id, text } = args
+	console.log(id, text)
+}) //create event
+logger.log('User logged!!')
